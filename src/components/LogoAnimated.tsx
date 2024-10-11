d5aabebd71c310c1e658d6e799530f78d155eb56
Martin RUPP-DAHLEM <mruppdahlem@gmail.com>
2024-10-11 19:25:16 +0200

    feat: ajout d'un moving the logo partiel

---
+++ src/components/LogoAnimated.tsx
@@ -0,0 +1,67 @@
+import "./LogoAnimated.css";
+import logo from "../assets/logo_thermidor_web_agency.png";
+import { useEffect, useState } from "react";
+const calculateTopValue = (): string => {
+  // Hauteur de la fenêtre (100vh)
+  const maxScroll = window.innerHeight;
+  console.log("MAX SCROLL: " + maxScroll);
+  console.log("SCROLL: " + window.scrollY);
+
+  // Valeurs de top en vh
+  const topStart = 35; // 35vh
+  const topEnd = 2; // 10vh
+
+  // Scroll actuel
+  const scrollY = window.scrollY;
+
+  // Clamp le scroll entre 0 et maxScroll (entre 0vh et 100vh)
+  const clampedScroll = Math.min(Math.max(scrollY, 0), maxScroll);
+
+  const interpolatedTop =
+    topStart - (topStart - topEnd) * (clampedScroll / maxScroll);
+
+  // Retourne la valeur 'top' en vh
+  return `${interpolatedTop}vh`;
+};
+
+const LogoAnimated: React.FC = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const [topPosition, setTopPosition] = useState("35vh");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setScrollY(scrollY);
+      setTopPosition(calculateTopValue());
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      // window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const logoScale = 1 - Math.min(scrollY / 400, 0.5);
+  return (
+    <>
+      <div className="underneathOverlay-logo">
+        <div
+          className={`container-logo`}
+          style={{
+            top: `${topPosition}`,
+            transform: ` scale(${logoScale})`,
+          }}
+        >
+          <img src={logo} alt="Logo Thermidor Agence Web Lille" />
+          <div className="text-logo-container">
+            <h1>THERMIDOR</h1>
+            <h1>AGENCE WEB</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LogoAnimated;
